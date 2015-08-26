ed6b7e347a85c250f7b41e76f6015eb782640b00
David Mohundro <david@mohundro.com>
2015-08-26 10:41:20 -0500

    Update install script to use PSGallery

--- install.ps1
+++ install.ps1
@@ -1,9 +1,6 @@
-# installs listed modules
-
-Install-Module pscx
 Install-Module Find-String
-Install-Module PSReadline
 Install-Module Jump.Location
-
-# not listed at PSGallery yet
-#installModule posh-git
+Install-Module posh-git
+Install-Module Pscx
+Install-Module PSReadline
+Install-Module PSScriptAnalyzer
